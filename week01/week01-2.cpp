e562403f84584fde956a3a5daa4f640c346cafaa
Jerfsy <ww6516ww22@gmail.com>
2023-02-14 12:00:30 +0800

    整理舊程式 加新的week01

---
+++ week01/week01-2.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    printf("請輸入一個字母: ");
+    char c;
+    scanf("%c", &c);
+    printf("你輸入了 %c \n", c);
+
+    printf("請輸入一個整數: ");
+    int a;
+    scanf("%d", &a);
+    printf("你輸入了 %d \n", a);
+    return 0;
+
+}
